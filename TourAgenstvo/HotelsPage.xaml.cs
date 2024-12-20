ef82e30419f571cc16f6fef656ba87ea113a29c0
lamuev <lamuev@308-9>
2024-12-20 12:26:24 +0800

    Добавьте файлы проекта.

---
+++ TourAgenstvo/HotelsPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TourAgenstvo
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelsPage.xaml
+    /// </summary>
+    public partial class HotelsPage : Page
+    {
+        public HotelsPage()
+        {
+            InitializeComponent();
+            // DGridHotels.ItemsSource = ToursBase1Entities1.GetContext().Hotel.ToList();
+        }
+private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {hotelsForRemoving.Count()} элементов?", "Внимание",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    ToursBaseYoEntities2.GetContext().Hotels.RemoveRange(hotelsForRemoving);
+                    ToursBaseYoEntities2.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+                    DGridHotels.ItemsSource = ToursBaseYoEntities2.GetContext().Hotels.ToList();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if ( Visibility == Visibility.Visible )
+            {
+                ToursBaseYoEntities2.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = ToursBaseYoEntities2.GetContext().Hotels.ToList();
+            }    
+        }
+    }
+}
---
+++ TourAgenstvo/LoginScreen.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TourAgenstvo
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginScreen : Page
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new ToursPage());
+        }
+        private bool IsValidadmin(string login, string password)
+        {
+
+            return login == "Admin" && password == "123";
+        }
+        private bool IsValidmanager(string login, string password)
+        {
+            return login == "Manager" && password == "1234";
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string login = dopUsername.Text;
+            string password = dopPassword.Password;
+
+
+            if (IsValidadmin(login, password))
+            {
+                MessageBox.Show("Добро пожаловать в систему Администратора!");
+                Manager.MainFrame.Navigate(new HotelsPage());
+
+            }
+            else if (IsValidmanager(login, password))
+            {
+                MessageBox.Show("Добро пожаловать в систему Менеджера");
+                Manager.MainFrame.Navigate(new AddEditPage(null));
+
+            }
+
+            else
+            {
+                MessageBox.Show("Неправильный пароль или логин.");
+            }
+        }
+    }
+}
